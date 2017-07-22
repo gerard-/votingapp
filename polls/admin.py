50e427971d19970a4aa8521ee014b296b3f5e57d
Gerard Krol <gerard@gerardkrol.nl>
2017-07-22 11:50:07 +0200

    Add Answers

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Question, Choice
+from .models import Question, Choice, Answer
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -14,4 +14,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
-#admin.site.register(Choice)
+admin.site.register(Answer)
