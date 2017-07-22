3271722d3905a7727c20989fa98d804cb4df1b82
Gerard Krol <gerard@gerardkrol.nl>
2017-07-22 23:03:42 +0200

    Add redirect from / to /polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http.response import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', lambda r: HttpResponseRedirect('polls/')),
 ]
