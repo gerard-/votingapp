0bb36aebdf0766c9244c6e317df89ddda86361b0
Gerard Krol <gerard@gerardkrol.nl>
2017-07-22 13:38:51 +0200

    Allow Questions to be copied

--- polls/admin.py
+++ polls/admin.py
@@ -8,10 +8,21 @@ from .models import Question, Choice, Answer
 class ChoiceInline(admin.TabularInline):
     model = Choice
 
+def copy_question(modeladmin, request, queryset):
+    for orig in queryset:
+        q = Question(question_text="Kopie van "+orig.question_text)
+        q.save()
+        for orig_choice in orig.choice_set.all():
+            c = Choice(question=q, choice_text=orig_choice.choice_text)
+            c.save()
+
+copy_question.short_description = "Kopieer stemmingen"
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [
         ChoiceInline,
     ]
+    actions = [copy_question]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
