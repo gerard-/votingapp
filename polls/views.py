4cf20a2de4e1a92e8d2ee3b8e1e00476b23ef7f2
Gerard Krol <gerard@gerardkrol.nl>
2017-10-23 22:58:00 +0200

    The prev button can now return to the first choice.

--- polls/views.py
+++ polls/views.py
@@ -187,7 +187,7 @@ def prev(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     qs = sorted(Question.objects.filter(question_category__in=[2,3]), key=sort_by_number)
     for i in range(0, len(qs)):
-        if str(qs[i].id) == str(question_id) and i-1 > 0:
+        if str(qs[i].id) == str(question_id) and i-1 >= 0:
             return HttpResponseRedirect(reverse('polls:results', args=(qs[i-1].id,)))
     return HttpResponseRedirect(reverse('polls:index'))
 
